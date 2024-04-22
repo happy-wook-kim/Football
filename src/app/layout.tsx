c5362dcccb704f91df5ac72afe2ee4b64cc7f7cd
jinwook <77241161+happy-wook-kim@users.noreply.github.com>
2024-04-22 16:50:20 +0900

    [FIX] root layout으로 속성 이전

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,9 @@ import { ReactNode } from 'react';
 interface ErrorProps {
   children: ReactNode;
 }
-
 export default function GlobalError({ children }: ErrorProps) {
   return (
-    <html>
+    <html lang="ko">
       <body>
         {children}
       </body>
