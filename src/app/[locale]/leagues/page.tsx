006e5a9efaf05af21d30da42dc37a50273356533
jinwook <77241161+happy-wook-kim@users.noreply.github.com>
2024-04-22 16:49:03 +0900

    [ADD] Submodule 파일에서 데이터 불러오기

--- src/app/[locale]/leagues/page.tsx
+++ src/app/[locale]/leagues/page.tsx
@@ -3,6 +3,7 @@
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../../../../lib/store"
 import { useRouter } from "next/navigation"
+import { keys } from "@/../keys/secert";
 
 export default function Leagues(){
   const count = useSelector((state:RootState) => state.counter.value)
@@ -12,6 +13,7 @@ export default function Leagues(){
   return (
     <div>
       {count}
+      <span>{keys.JS}</span>
       <button onClick={() => router.back()}>BACK</button>
     </div>
   )
